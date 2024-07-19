90c84e904a8852190527209b0d84627ef1402ebb
muameh <balmehmet111@gmail.com>
2024-07-19 23:36:34 +0300

    simple UI was completed

--- app/src/main/java/com/mehmetbaloglu/unitconverter/MainActivity.kt
+++ app/src/main/java/com/mehmetbaloglu/unitconverter/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.mehmetbaloglu.unitconverter
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,20 +8,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.mehmetbaloglu.unitconverter.ui.theme.UnitConverterTheme
@@ -44,39 +45,71 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun UnitConverter() {
-    Column (
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
+    Column(
+        verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally
+    ) {
         Text(
-            text = "Unit Converter",
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(16.dp))
+            text = "Unit Converter", modifier = Modifier.align(Alignment.CenterHorizontally)
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         OutlinedTextField(value = "", onValueChange = { })
 
-        Row (modifier = Modifier.padding(16.dp)){
+        Row(modifier = Modifier.padding(16.dp)) {
 
-            Box {
+            Box(modifier = Modifier.padding(5.dp)) {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
-                    Icon(
-                        Icons.Default.ArrowDropDown,
-                        contentDescription = null)
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "cm")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "m")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "mm")},
+                        onClick = { /*TODO*/ }
+                    )
 
                 }
             }
 
-            Box {
+
+            Box(modifier = Modifier.padding(5.dp)) {
                 Button(onClick = { /*TODO*/ }) {
                     Text(text = "Select")
-                    Icon(
-                        Icons.Default.ArrowDropDown,
-                        contentDescription = null)
+                    Icon(Icons.Default.ArrowDropDown, contentDescription = null)
+                }
+                DropdownMenu(expanded = false, onDismissRequest = { /*TODO*/ }) {
+                    DropdownMenuItem(
+                        text = { Text(text = "cm")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "m")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "Feet")},
+                        onClick = { /*TODO*/ }
+                    )
+                    DropdownMenuItem(
+                        text = { Text(text = "mm")},
+                        onClick = { /*TODO*/ }
+                    )
 
                 }
             }
-
 
         }
 
@@ -84,7 +117,6 @@ fun UnitConverter() {
 
     }
 }
-
 
 
 @Preview(showBackground = true)
